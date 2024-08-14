1f40a63379610ffb44ff790a9d4e1c5c0b14a4dc
Felix Goyeh <felix.goyeh@student.moringaschool.com>
2024-08-14 16:48:47 +0300

    completed code structure for ArtDetails component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Contacts  from './Pages/Contacts';
-
+import Contact from './components/Contact'
+import './App.css'
+  
 function App() {
     return (
       <div>
-       <Contacts />
+       <Contact />
   </div>
     );
   }
---
+++ src/Components/ArtDetails.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function ArtDetail({ artworks, onLike, onAddToCart }) {
+  const { id } = useParams();
+  const art = artworks.find((art) => art.id === id); // Check for string IDs
+
+  if (!art) {
+    return <p>Art not found!</p>;
+  }
+
+  const handleLike = () => {
+    onLike(art.id);
+  };
+
+  const handleAddToCart = () => {
+    onAddToCart(art.id);
+  };
+
+  return (
+    <div>
+      <h2>{art.title}</h2>
+      <img src={art.image} alt={art.title} />
+      <p>Artist: {art.artist}</p>
+      <p>Year: {art.year}</p>
+      <p>Price: ${art.price}</p>
+      <p>Details: {art.detail}</p>
+      <p>Likes: {art.likes}</p>
+      <button onClick={handleLike}>Like</button>
+      <button onClick={handleAddToCart} disabled={art.sold}>
+        {art.sold ? "Sold Out" : "Add to Cart"}
+      </button>
+    </div>
+  );
+}
+
+export default ArtDetail;
