6e9cdd9f025a1475662644db5a6b88f5bb2fb001
Felix Goyeh <felix.goyeh@student.moringaschool.com>
2024-08-14 10:35:07 +0300

    created the contact us component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Routes } from 'react-router-dom'
-import Home from './Components/Home';
-import About from './Components/About';
-import Portraits from './Components/Portraits';
-import Documentary from './Components/Documentary';
-import Albums from './Components/Albums';
-import Blog from './Components/Blog';
+import Contacts  from './Pages/Contacts';
 
 function App() {
     return (
       <div>
-        <BrowserRouter>
-  
-        <Routes>
-          <Route path='/Home' element={<Home/>}/>
-          <Route path='/About' element={<About category="About"/>}/>
-          <Route path='/Portraits' element={<Portraits category="Portraits"/>}/>
-          <Route path='/Documentary' element={<Documentary category="Documentary"/>}/>
-          <Route path='/Albums' element={<Albums/>}/>
-          <Route path='/Blog' element={<Blog/>}/>
-        </Routes>
-  
-   
-    </BrowserRouter>
+       <Contacts />
   </div>
     );
   }
