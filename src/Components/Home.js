aea13856c9f1cc71baefe06b0b9225a836d30e68
fiona <fiona.mburu@student.moringaschool.com>
2024-08-15 01:02:28 +0300

    adds home component

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Virtual Art Gallery</h1>
+      <img src="" alt="Art Gallery" />
+    </div>
+  );
+}
+
+export default Home;
