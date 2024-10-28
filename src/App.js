1697ef9f66f69fe50620f1096a024557edb27a0e
Felix Goyeh <felix.goyeh@student.moringaschool.com>
2024-10-28 10:29:31 +0300

    changed the backened link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./Components/Navbar";
+// import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Gallery from "./Components/Gallery";
 import AddArt from "./Components/AddArt";
@@ -13,10 +13,17 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    fetch("https://artt-backend.vercel.app/artworks")
-      .then((response) => response.json())
-      .then((data) => setArtworks(data));
+    fetch("https://phase-2-final-project-sbww.onrender.com/artworks")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => setArtworks(data))
+      .catch((error) => console.error('Fetch error:', error));
   }, []);
+  
 
   const addArt = (newArt) => {
     setArtworks([...artworks, newArt]);
@@ -27,7 +34,7 @@ function App() {
       art.id === id ? { ...art, likes: art.likes + 1 } : art
     );
     setArtworks(updatedArtworks);
-    fetch(`https://artt-backend.vercel.app/artworks/${id}`, {
+    fetch(`https://phase-2-final-project-1.onrender.com/artworks/${id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -43,7 +50,7 @@ function App() {
       setArtworks(
         artworks.map((a) => (a.id === id ? { ...a, sold: true } : a))
       );
-      fetch(`https://artt-backend.vercel.app/artworks/${id}`, {
+      fetch(`https://phase-2-final-project-1.onrender.com/artworks/${id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ sold: true }),
@@ -54,7 +61,7 @@ function App() {
   const removeFromCart = (id) => {
     setCart(cart.filter((item) => item.id !== id));
     setArtworks(artworks.map((a) => (a.id === id ? { ...a, sold: false } : a)));
-    fetch(`https://artt-backend.vercel.app/artworks/${id}`, {
+    fetch(`https://phase-2-final-project-1.onrender.com/artworks/${id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ sold: false }),
