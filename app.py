3628b1bfa439091593e26b79f933618af49db0c1
Felix Goyeh <felix.goyeh@student.moringaschool.com>
2024-10-29 09:36:48 +0300

    added flask cors

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 from models import db, User, Post, Artwork, Artist
-# from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 migrate = Migrate(app, db)
